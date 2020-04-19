5d6741adf78938b9a761a16710ab89686e6bb086
Kranti Kumar R <46646270+krantirk@users.noreply.github.com>
2020-04-19 17:04:05 +0530

    Add files via upload

---
+++ sandesh/send.py
@@ -0,0 +1,45 @@
+"""
+A simple slack message sender
+@author: Kranti Kumar (jagannathrk2020@gmail.com)
+"""
+import collections
+import json
+import os
+import requests
+
+
+def send(msg, use_raw=False, webhook=None):
+    """
+    A function to send messages on slack
+    :param msg: string, list of strings, dictionary (or defaultdict/OrderedDict)
+    :param use_raw: if True, msg (dict) is dumped as json and sent to slack. Use for advanced messages
+    :param webhook: Slack webhook URL. Either this or SANDESH_WEBHOOK environment variable should be present
+    """
+    if webhook is None:
+        webhook = os.environ.get("SANDESH_WEBHOOK")
+    
+    if webhook is None:
+        raise Exception("No webhooks found")
+    
+    data = dict()
+    
+    if isinstance(msg, str):
+        data["text"] = msg
+        requests.post(webhook, json.dumps(data))
+    
+    elif isinstance(msg, list):
+        data["text"] = '\n'.join(msg)
+        requests.post(webhook, json.dumps(data))
+    
+    elif isinstance(msg, (dict, collections.defaultdict, collections.OrderedDict)):
+        values = []
+        for k, v in msg.items():
+            values.append(str(k) + ": " + str(v))
+        data["text"] = '\n'.join(values)
+        requests.post(webhook, json.dumps(data))
+    
+    elif isinstance(msg, dict) and use_raw is True:
+        requests.post(webhook, json.dumps(msg))
+    
+    else:
+        raise Exception("Type for variable: msg, is currently not supported")
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, Extension
+from setuptools import find_packages
+
+import sandesh
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+if __name__ == "__main__":
+    setup(
+        name="sandesh",
+        version=sandesh.__version__,
+        description="sandesh - a simple app to send messages on slack",
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        author="Kranti Kumar",
+        author_email="jagannathrk2020@gmail.com",
+        url="https://github.com/krantirk",
+        license="MIT License",
+        packages=find_packages(),
+        include_package_data=True,
+        platforms=["linux", "unix"],
+        python_requires='>3.5.2'
+    )
